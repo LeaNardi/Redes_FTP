bc70b641b8789507c56155be07899dcb1a2b61d5
LeaNardi <leandroenardi@gmail.com>
2021-06-19 18:58:55 -0300

    Clase 2021-06-15: Comandos get, put, port

--- ftpclient.c
+++ ftpclient.c
@@ -32,7 +32,7 @@ bool recv_msg(int sd, int code, char *text) {
 
     // error checking
     if (recv_s < 0) warn("Error receiving data");
-    if (recv_s == 0) errx(4, "Connection closed by host");
+    if (recv_s == 0) errx(7, "Connection closed by host");
 
     // parsing the code and message receive from the answer
     sscanf(buffer, "%d %[^\r\n]\r\n", &recv_code, message);
@@ -60,7 +60,7 @@ void send_msg(int sd, char *operation, char *param) {
 
     // send command and check for errors
     write(sd, buffer, strlen(buffer)+1);
-
+    return;
 }
 
 /**
@@ -80,8 +80,7 @@ char * read_input() {
  * sd: socket descriptor
  **/
 void authenticate(int sd) {
-    char *input, desc[100];
-    int code;
+    char *input;
 
     // ask for user
     printf("username: ");
@@ -95,7 +94,7 @@ void authenticate(int sd) {
 
     // wait to receive password requirement and check for errors
     if(!recv_msg(sd, 331, NULL))
-        errx(5, "Invalid User");
+        errx(8, "Invalid User");
 
     // ask for password
     printf("password: ");
@@ -109,41 +108,209 @@ void authenticate(int sd) {
 
     // wait for answer and process it and check for errors
     if(recv_msg(sd, 530, NULL))
-        errx(6, "Incorrect login");
-
+        errx(9, "Incorrect login");
+    return;
 }
+
+
+bool port(int sd, char* ip, int puerto){
+    char buffer[BUFSIZE];
+    int i;
+    char *port_param, *aux;
+    port_param = (char*)malloc(30*sizeof(char));
+    aux = (char*)malloc(8*sizeof(char));
+
+    strcpy(port_param, ip);
+
+    i=0;
+    while(*(port_param+i) !='\0'){
+        if (*(port_param+i) == '.') *(port_param+i) = ',';
+        i++;
+    }
+
+    sprintf(aux, ",%d,%d",puerto/256, puerto%256);
+    strcat(port_param, aux);
+
+    send_msg(sd, "PORT", port_param);
+
+    free(port_param);
+    free(aux);
+
+    // check for the response and return
+    return recv_msg(sd, 200, buffer);
+}
+
+
+
+
 
 /**
  * function: operation get
  * sd: socket descriptor
  * file_name: file name to get from the server
  **/
-void get(int sd, char *file_name) {//por ahora no
-    char desc[BUFSIZE], buffer[BUFSIZE];
-    int f_size, recv_s, r_size = BUFSIZE;
+void get(int sd, char *file_name) {
+    char buffer[BUFSIZE];
+    long f_size, recv_s, r_size = BUFSIZE;
     FILE *file;
+    int dsd, dsda;// data channel socket
+    struct sockaddr_in addr, addr2;
+    socklen_t addr_len = sizeof(addr);
+    socklen_t addr2_len = sizeof(addr2);
+    int puerto;
+    char *ip;
+    ip = (char*)malloc(13*sizeof(char));
+
+    getsockname(sd, (struct sockaddr *) &addr, &addr_len);
+    ip = inet_ntoa(addr.sin_addr);
+    puerto = rand()%60000+1024;
+
+    if(!port(sd, ip, puerto)) {
+       printf("Invalid server answer\n");
+       return;
+    }
+
+      // listen to data channel (default idem port)
+    dsd = socket(AF_INET, SOCK_STREAM, 0);
+    if (dsd < 0) errx(2, "Cannot create socket");
+    addr2.sin_family = AF_INET;
+    addr2.sin_addr.s_addr = INADDR_ANY;
+    addr2.sin_port = htons(puerto);
+    if (bind(dsd, (struct sockaddr *) &addr2, sizeof(addr2)) < 0) errx(4,"Cannot bind");
+    if (listen(dsd,1) < 0) errx(5, "Listen data channel error");
 
     // send the RETR command to the server
-
+    send_msg(sd, "RETR", file_name);
     // check for the response
+    if(!recv_msg(sd, 299, buffer)) {
+       close(dsd);
+       return;
+    }
+
+    // accept new connection
+    dsda = accept(dsd, (struct sockaddr*)&addr2, &addr2_len);
+    if (dsda < 0) {
+       errx(6, "Accept data channel error");
+    }
 
     // parsing the file size from the answer received
     // "File %s size %ld bytes"
-    sscanf(buffer, "File %*s size %d bytes", &f_size);
+    sscanf(buffer, "File %*s size %ld bytes", &f_size);
 
     // open the file to write
     file = fopen(file_name, "w");
 
     //receive the file
+    while(true) {
+       if (f_size < BUFSIZE) r_size = f_size;
+       recv_s = read(dsda, buffer, r_size);
+       if(recv_s < 0) warn("receive error");
+       fwrite(buffer, 1, r_size, file);
+       if (f_size < BUFSIZE) break;
+       f_size = f_size - BUFSIZE;
+    }
 
-
+    // close data channel
+    close(dsda);
 
     // close the file
     fclose(file);
 
     // receive the OK from the server
+    if(!recv_msg(sd, 226, NULL)) warn("Abnormally RETR terminated");
 
+    // close listening socket
+    close(dsd);
+
+    return;
 }
+/**
+ * function put
+ **/
+
+ void put(int sd, char *file_name) {
+    char buffer[BUFSIZE];
+    long f_size;
+    FILE *file;
+    int dsd, dsda;// data channel socket
+    struct sockaddr_in addr, addr2;
+    socklen_t addr_len = sizeof(addr);
+    socklen_t addr2_len = sizeof(addr2);
+    int puerto, bread;
+    char *ip;
+    char *file_data, *file_size;
+    file_data = (char*)malloc(50*sizeof(char));
+    file_size = (char*)malloc(25*sizeof(char));
+
+    // check if file exists if not inform error to client
+    file = fopen(file_name, "r");
+    if (file == NULL){
+        printf("El archivo no existe.\n");
+        return;
+    }
+
+    //file length
+    fseek(file, 0L, SEEK_END);
+    f_size = ftell(file);
+    rewind(file);
+    sprintf(file_size, "//%ld",f_size);
+
+
+    ip = (char*)malloc(13*sizeof(char));
+    getsockname(sd, (struct sockaddr *) &addr, &addr_len);
+    ip = inet_ntoa(addr.sin_addr);
+    puerto = rand()%60000+1024;
+
+    if(!port(sd, ip, puerto)) {
+       printf("Invalid server answer\n");
+       return;
+    }
+
+    // listen to data channel (default idem port)
+    dsd = socket(AF_INET, SOCK_STREAM, 0);
+    if (dsd < 0) errx(1, "Cannot create socket");
+    addr2.sin_family = AF_INET;
+    addr2.sin_addr.s_addr = INADDR_ANY;
+    addr2.sin_port = htons(puerto);
+    if (bind(dsd, (struct sockaddr *) &addr2, sizeof(addr2)) < 0) errx(4,"Cannot bind");
+    if (listen(dsd,1) < 0) errx(5, "Listen data channel error");
+
+    file_data=strcat(file_name,file_size);
+    // send the STOR command to the server
+    send_msg(sd, "STOR", file_data);
+    // check for the response
+    if(!recv_msg(sd, 150, buffer)) {
+       close(dsd);
+       return;
+    }
+
+    // accept new connection
+    dsda = accept(dsd, (struct sockaddr*)&addr2, &addr2_len);
+    if (dsda < 0) {
+       errx(6, "Accept data channel error");
+    }
+
+    // send the file
+    while(!feof(file)) {
+        bread = fread(buffer, 1, BUFSIZE, file);
+        if (write(dsda, buffer, bread) < 0) warn("Error sending data");
+    }
+
+    // close data channel
+    close(dsda);
+
+    // close the file
+    fclose(file);
+
+    // receive the OK from the server
+    if(!recv_msg(sd, 226, NULL)) warn("Abnormally RETR terminated");
+
+    // close listening socket
+    close(dsd);
+
+    return;
+}
+
 
 /**
  * function: operation quit
@@ -154,7 +321,8 @@ void quit(int sd) {
     send_msg(sd, "QUIT", NULL);
     // receive the answer from the server
     if(!recv_msg(sd, 221, NULL))
-        errx(7, "Incorrect logout");
+        errx(10, "Incorrect logout");
+    return;
 }
 
 /**
@@ -170,10 +338,13 @@ void operate(int sd) {
         if (input == NULL)
             continue; // avoid empty input
         op = strtok(input, " ");
-        // free(input);
         if (strcmp(op, "get") == 0) {
-            //param = strtok(NULL, " ");
-            //get(sd, param);
+            param = strtok(NULL, " ");
+            get(sd, param);
+        }
+        else if (strcmp(op, "put") == 0) {
+            param = strtok(NULL, " ");
+            put(sd, param);
         }
         else if (strcmp(op, "quit") == 0) {
             quit(sd);
@@ -183,7 +354,6 @@ void operate(int sd) {
             // new operations in the future
             printf("TODO: unexpected command\n");
         }
-        free(input);
     }
     free(input);
 }
@@ -243,8 +413,6 @@ int main (int argc, char *argv[]) {
     if(!direccion_puerto(argv[2]))
         errx(1, "Invalidad Port");
 
-
-
     // create socket and check for errors
     sd = socket(AF_INET, SOCK_STREAM, 0);
     if (sd < 0) {
@@ -264,11 +432,8 @@ int main (int argc, char *argv[]) {
     // if receive hello proceed with authenticate and operate if not warning
     //construir recvmsg
     if(recv_msg(sd, 220, NULL)) {
-        //printf("Llego hasta acá\n");
         authenticate(sd);
         operate(sd);
-        //printf("Llego hasta acá\n");
-
 
     } else{
         warnx("Didn't receive data from server");
--- ftpserver.c
+++ ftpserver.c
@@ -8,10 +8,14 @@
 #include <err.h>
 
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <signal.h>
+#include <sys/wait.h>
 
 #define BUFSIZE 512
 #define CMDSIZE 4
 #define PARSIZE 100
+#define SRCPORT 20
 
 #define MSG_220 "220 srvFtp version 1.0\r\n"
 #define MSG_331 "331 Password required for %s\r\n"
@@ -21,6 +25,8 @@
 #define MSG_550 "550 %s: no such file or directory\r\n"
 #define MSG_299 "299 File %s size %ld bytes\r\n"
 #define MSG_226 "226 Transfer complete\r\n"
+#define MSG_200 "200 PORT command successful\r\n"
+#define MSG_150 "150 Opening BINARY mode data connection for %s\r\n"
 
 //Auxiliar libraries
 #include<ctype.h>
@@ -58,7 +64,7 @@ bool recv_cmd(int sd, char *operation, char *param) {
     // extract command receive in operation if not set \0
     // extract parameters of the operation in param if it needed
     token = strtok(buffer, " ");
-    if (token == NULL || strlen(token) < 4) {
+    if (token == NULL || strlen(token) != CMDSIZE) {
         warn("not valid ftp command");
         return false;
     } else {
@@ -103,27 +109,98 @@ bool send_ans(int sd, char *message, ...){
  * sd: socket descriptor
  * file_path: name of the RETR file
  **/
-
- /*
-void retr(int sd, char *file_path) {
+void retr(int sd, struct sockaddr_in addr, char *file_path) {
     FILE *file;
     int bread;
     long fsize;
     char buffer[BUFSIZE];
+    int srcsd;
 
     // check if file exists if not inform error to client
+    file = fopen(file_path, "r");
+    if (file == NULL){
+        send_ans(sd, MSG_550, file_path);
+        return;
+    }
 
     // send a success message with the file length
+    fseek(file, 0L, SEEK_END);
+    fsize = ftell(file);
+    rewind(file);
+    send_ans(sd, MSG_299, file_path, fsize);
+
+    // open connection to client
+    srcsd = socket(AF_INET, SOCK_STREAM, 0);
+    if (srcsd < 0) errx(2, "Cannot create socket");
+
+    if (connect(srcsd, (struct sockaddr *) &addr, sizeof(addr)) < 0) errx(3, "Error on connect to data channel");
 
     // important delay for avoid problems with buffer size
-    sleep(1);
+    //sleep(1);
 
     // send the file
+    while(!feof(file)) {
+        bread = fread(buffer, 1, BUFSIZE, file);
+        if (write(srcsd, buffer, bread) < 0) warn("Error sending data");
+    }
 
-    // close the file
+    // close connection to client
+    close(srcsd);
 
     // send a completed transfer message
-}*/
+    send_ans(sd, MSG_226);
+    return;
+}
+/**
+ * function stor
+ **/
+
+ void stor(int sd, struct sockaddr_in addr, char *file_data) {
+    FILE *file;
+    long f_size, recv_s, r_size = BUFSIZE;
+    char buffer[BUFSIZE];
+    int srcsd;
+    char *file_path, *file_size, *aux;
+    file_path = (char*)malloc(50*sizeof(char));
+    file_size = (char*)malloc(25*sizeof(char));
+
+    //Extract file name and file size
+    aux = strtok(file_data,"//");
+    strcpy(file_path,aux);
+    aux = strtok(NULL,"//");
+    strcpy(file_size,aux);
+    f_size = atoi(file_size);
+
+    send_ans(sd, MSG_150, file_path);
+    // open connection to client
+    srcsd = socket(AF_INET, SOCK_STREAM, 0);
+    if (srcsd < 0) errx(2, "Cannot create socket");
+
+    if (connect(srcsd, (struct sockaddr *) &addr, sizeof(addr)) < 0) errx(3, "Error on connect to data channel");
+
+    // open the file to write
+    file = fopen(file_path, "w");
+
+     //receive the file
+    while(true) {
+       if (f_size < BUFSIZE) r_size = f_size;
+       recv_s = read(srcsd, buffer, r_size);
+       if(recv_s < 0) warn("receive error");
+       fwrite(buffer, 1, r_size, file);
+       if (f_size < BUFSIZE) break;
+       f_size = f_size - BUFSIZE;
+    }
+
+    // close connection to client
+    close(srcsd);
+
+    // send a completed transfer message
+    send_ans(sd, MSG_226);
+    return;
+}
+
+
+
 /**
  * funcion: check valid credentials in ftpusers file
  * user: login user name
@@ -206,24 +283,93 @@ bool authenticate(int sd) {
     }
 }
 
+
+struct sockaddr_in port(int sd, char *socketdata){
+    struct sockaddr_in addr;
+    int puerto, i, j, count;
+    char *ip, *aux1, *aux2;
+    ip = (char*)malloc(30*sizeof(char));
+    aux1 = (char*)malloc(4*sizeof(char));
+    aux2 = (char*)malloc(4*sizeof(char));
+
+    i = j = 0;
+    count=0;
+
+    while(true){
+        if (*(socketdata+i) == ',') count++;
+
+        if (count<=3){
+            *(ip+j) = *(socketdata+i);
+            if (*(ip+j) == ',') *(ip+j) = '.';
+            j++;
+        }
+        if (*(socketdata+i) == ',' && count==4){
+            *(ip+j) = '\0';
+            j=0;
+            i++;
+            continue;
+        }
+
+        if(count==4){
+            *(aux1+j) = *(socketdata+i);
+            j++;
+        }
+
+        if (*(socketdata+i) == ',' && count==5){
+            *(aux1+j) = '\0';
+            j=0;
+            i++;
+            continue;
+        }
+
+        if(count==5){
+            *(aux2+j) = *(socketdata+i);
+            j++;
+        }
+
+        if (*(socketdata+i) == '\0'){
+            *(aux2+j) = '\0';
+            break;
+        }
+        i++;
+    }
+    puerto = 256 * atoi(aux1) + atoi(aux2);
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr(ip);
+    addr.sin_port = htons(puerto);
+
+    free(ip);
+    free(aux1);
+    free(aux2);
+
+    send_ans(sd, MSG_200);
+
+    return addr;
+}
+
+
 /**
  *  function: execute all commands (RETR|QUIT)
  *  sd: socket descriptor
  **/
 
 void operate(int sd) {
-    char op[CMDSIZE], param[PARSIZE];
+    char op[CMDSIZE+1], param[PARSIZE];
+    struct sockaddr_in client_address;
 
     while (true) {
         op[0] = param[0] = '\0';
         // check for commands send by the client if not inform and exit
         if(!recv_cmd(sd, op, param)){
             warnx("Anormally flow");
-            continue;
+            break;
         }
-
-        if (strcmp(op, "RETR") == 0) {
-            //retr(sd, param);
+        if (strcmp(op, "PORT") == 0 ){
+            client_address = port(sd, param);
+        }else if (strcmp(op, "RETR") == 0) {
+            retr(sd, client_address, param);
+        }else if (strcmp(op, "STOR") == 0) {
+            stor(sd, client_address, param);
         } else if (strcmp(op, "QUIT") == 0) {
             // send goodbye and close connection
             send_ans(sd, MSG_221);
@@ -232,8 +378,8 @@ void operate(int sd) {
             break;
         } else {
             // invalid command
-            warnx("Not supported command");
-            // furute use
+            warnx("Not supported command: %s\n", op);
+            // future use
         }
     }
 }
@@ -241,6 +387,8 @@ void operate(int sd) {
 
 
 //Auxiliar functions
+
+
 bool direccion_puerto(char *string){
     bool verificacion = true;
     int i=0;
@@ -253,11 +401,22 @@ bool direccion_puerto(char *string){
 }
 
 
+void sig_handler(int sig){
+    if(sig==SIGCHLD){
+        waitpid(-1, NULL, WNOHANG);
+    }
+}
+
 /**
  * Run with
  *         ./mysrv <SERVER_PORT>
  **/
+
 int main (int argc, char *argv[]) {
+    // reserve sockets and variables space
+    int msd, ssd;//master(server) sd and slave (client) sd
+    struct sockaddr_in m_addr, s_addr;
+    socklen_t s_addr_len;
 
     // arguments checking
     if(argc!=2){
@@ -267,16 +426,10 @@ int main (int argc, char *argv[]) {
         errx(1, "Invalidad Port");
 
 
-    // reserve sockets and variables space
-    int msd, ssd;//master(server) sd and slave (client) sd
-    struct sockaddr_in m_addr, s_addr;
-    socklen_t s_addr_len;
-
-
     // create server socket and check errors
     msd = socket(AF_INET, SOCK_STREAM, 0);
     if (msd < 0) {
-        errx(1, "Can't create server socket");
+        errx(2, "Can't create server socket");
     }
 
     // bind master socket and check errors
@@ -285,36 +438,46 @@ int main (int argc, char *argv[]) {
     m_addr.sin_port = htons(atoi(argv[1])); //server port
 
     if(bind(msd, (struct sockaddr *)&m_addr, sizeof(m_addr)) < 0) {
-      errx(2, "Can't bind socket");
+      errx(4, "Can't bind socket");
     }
 
     // make it listen
     if(listen(msd, 10) < 0) {
-        errx(3, "Listen error");
+        errx(5, "Error on listen");
     }
 
     // main loop
     while (true) {
+        pid_t pid_hijo;
         // accept connectiones sequentially and check errors
         s_addr_len = sizeof(s_addr);
         ssd = accept(msd, (struct sockaddr *)&s_addr, &s_addr_len);
+
         if (ssd < 0) {
-            errx(4, "Accept error");
+            errx(6, "Accept error");
         }
-        #if DEBUG
-        printf("Cliente se conecto\n");
-        #endif // DEBUG
 
-        // send hello
-        send_ans(ssd, MSG_220);
+        signal(SIGCHLD, sig_handler);
 
-        // operate only if authenticate is true
-        if(authenticate(ssd))
-            operate(ssd);
-        else{
-            warnx("Connection closed");
+        pid_hijo = fork();
+        if (pid_hijo==0){ //child process
+            // close the master socket
+            close(msd);
+
+            // send hello
+            send_ans(ssd, MSG_220);
+
+            // operate only if authenticate is true
+            if(authenticate(ssd))
+                operate(ssd);
+            else{
+                warnx("Connection closed in child %d ", getpid());
+            }
             close(ssd);
+            exit(0);
         }
+        //parent process
+        close(ssd);
 
     }
 
